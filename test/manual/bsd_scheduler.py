d7785d1c8bc0190e87a729dc4e490da89bf40d22
Granha <fegranha@gmail.com>
2016-11-26 20:53:29 -0600

    Data Test and Images

--- test/manual/bsd_scheduler.py
+++ test/manual/bsd_scheduler.py
@@ -1,4 +1,5 @@
 from scheduler.bsd_scheduler import BSDScheduler
+from scheduler.round_robin import RoundRobin
 
 from simulator.simulator import Simulator
 from simulator.workload_generator import WorkloadGenerator
@@ -23,8 +24,8 @@ if __name__ == '__main__':
     # simulator.run()                             
     
     
-    wGen = WorkloadGenerator(num_jobs=500, rate_jobs=5, rate_io=0.3,
-                             mean_job_time=1000, mean_io_time=3,
+    wGen = WorkloadGenerator(num_jobs=2000, rate_jobs=5, rate_io=0.3,
+                             mean_job_time=100, mean_io_time=3,
                              sd_job_time=5, sd_io_time=1, time_unit=1)
 
     ##############################
@@ -35,6 +36,9 @@ if __name__ == '__main__':
     workload1 = wGen.generateWorkload()
     
     workload2 = copy.deepcopy(workload1)
+    workload3 = copy.deepcopy(workload1)
+    workload4 = copy.deepcopy(workload1)
+
     simulator.setWorkload(workload1)    
 
     if DO_PROFILING:
@@ -56,3 +60,23 @@ if __name__ == '__main__':
     else:
         simulator.run()
 
+    ##############################
+    #   Simulate with Euler
+    ##############################
+    scheduler = RoundRobin(enableEuler=True)    
+    simulator = Simulator(scheduler)
+    workload1 = wGen.generateWorkload()
+    
+    workload2 = copy.deepcopy(workload1)
+    simulator.setWorkload(workload3)
+    simulator.run()
+
+    ##############################
+    #   Simulate without Euler
+    ##############################    
+    scheduler = RoundRobin(enableEuler=False)
+    
+    simulator = Simulator(scheduler)
+
+    simulator.setWorkload(workload4)
+    simulator.run()
